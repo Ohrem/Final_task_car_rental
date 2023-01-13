20a4c0ae41364da6ffd84bb2342697d52913ed1d
Romashka <ohrem25032002@gmail.com>
2023-01-13 17:10:38 +0300

    payment service

--- parent-project/module3/src/main/java/my/ohrem/service/service/PaymentService.java
+++ parent-project/module3/src/main/java/my/ohrem/service/service/PaymentService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service //TODO add all CRUD methods
 public class PaymentService {
 
@@ -15,5 +17,20 @@ public class PaymentService {
     @Transactional
     public void update(PaymentEntity paymentEntity) {
         paymentEntityDao.update(paymentEntity);
+    }
+
+    @Transactional
+    public void delete(PaymentEntity paymentEntity) {
+        paymentEntityDao.delete(paymentEntity);
+    }
+
+    @Transactional
+    public void create(PaymentEntity paymentEntity) {
+        paymentEntityDao.create(paymentEntity);
+    }
+
+    @Transactional
+    public List<PaymentEntity> readAll() {
+        return paymentEntityDao.readAll();
     }
 }
